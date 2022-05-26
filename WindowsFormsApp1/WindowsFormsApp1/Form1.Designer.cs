6688be27a6f8f8726bb7aa66008e70d738a4d380
Hokkerok <81930081+Hokkerok@users.noreply.github.com>
2022-05-26 23:37:33 +0300

    asdfpaoh

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.X = new System.Windows.Forms.Label();
+            this.Y = new System.Windows.Forms.Label();
+            this.textboxAX = new System.Windows.Forms.TextBox();
+            this.textboxAY = new System.Windows.Forms.TextBox();
+            this.Graph = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.drawButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Graph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loadButton.Location = new System.Drawing.Point(12, 492);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(292, 37);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 171);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(279, 306);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(315, 492);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Server";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Database";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "User";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Passvord";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Table";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(296, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Query";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(87, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(87, 65);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(87, 95);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(87, 123);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 16;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(299, 34);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(175, 22);
+            this.textBox6.TabIndex = 17;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(492, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Find";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // X
+            // 
+            this.X.AutoSize = true;
+            this.X.Location = new System.Drawing.Point(352, 232);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(17, 17);
+            this.X.TabIndex = 24;
+            this.X.Text = "X";
+            this.X.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // Y
+            // 
+            this.Y.AutoSize = true;
+            this.Y.Location = new System.Drawing.Point(352, 261);
+            this.Y.Name = "Y";
+            this.Y.Size = new System.Drawing.Size(17, 17);
+            this.Y.TabIndex = 25;
+            this.Y.Text = "Y";
+            this.Y.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // textboxAX
+            // 
+            this.textboxAX.Location = new System.Drawing.Point(385, 229);
+            this.textboxAX.Name = "textboxAX";
+            this.textboxAX.Size = new System.Drawing.Size(100, 22);
+            this.textboxAX.TabIndex = 26;
+            this.textboxAX.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // textboxAY
+            // 
+            this.textboxAY.Location = new System.Drawing.Point(385, 255);
+            this.textboxAY.Name = "textboxAY";
+            this.textboxAY.Size = new System.Drawing.Size(100, 22);
+            this.textboxAY.TabIndex = 27;
+            this.textboxAY.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // Graph
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.Graph.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.Graph.Legends.Add(legend1);
+            this.Graph.Location = new System.Drawing.Point(315, 336);
+            this.Graph.Name = "Graph";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.Graph.Series.Add(series1);
+            this.Graph.Size = new System.Drawing.Size(300, 141);
+            this.Graph.TabIndex = 28;
+            this.Graph.Text = "chart1";
+            this.Graph.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // drawButton
+            // 
+            this.drawButton.Location = new System.Drawing.Point(385, 293);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(75, 23);
+            this.drawButton.TabIndex = 29;
+            this.drawButton.Text = "Draw";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 551);
+            this.Controls.Add(this.drawButton);
+            this.Controls.Add(this.Graph);
+            this.Controls.Add(this.textboxAY);
+            this.Controls.Add(this.textboxAX);
+            this.Controls.Add(this.Y);
+            this.Controls.Add(this.X);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.loadButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.resizeContent);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Graph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label X;
+        private System.Windows.Forms.Label Y;
+        private System.Windows.Forms.TextBox textboxAX;
+        private System.Windows.Forms.TextBox textboxAY;
+        private System.Windows.Forms.DataVisualization.Charting.Chart Graph;
+        private System.Windows.Forms.Button drawButton;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,172 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Database DB;
+        private DataTable table;
+        private MySqlDataAdapter adapter;
+
+        class Database 
+        {
+            public String serverString;
+            public String databaseString;
+            public String userString;
+            public String passwordString;
+            public String tableString;
+            public String connectionString;
+
+            MySqlConnection connection;
+            //= new MySqlConnection("Server=localhost; Database=turizm; User ID=root; Password=08ezozeb");
+            public void buildConnectionString() 
+            {
+                connectionString = "Server=" + serverString + "; Database=" + databaseString + "; Password=" + passwordString;
+                connection = new MySqlConnection(connectionString);
+            }
+
+            public void openConnection()
+            {
+                if (connection.State == System.Data.ConnectionState.Closed) connection.Open();
+            }
+
+            public void closeConnection() 
+            {
+                if(connection.State==System.Data.ConnectionState.Open) { connection.Close(); }
+            }
+
+            public MySqlConnection GetSqlConnection() 
+            {
+                return connection;
+            }
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            DB = new Database();
+            table = new DataTable();
+            adapter = new MySqlDataAdapter();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            clearAll();
+            DB.serverString = textBox1.Text;
+            DB.databaseString = textBox2.Text;
+            DB.userString = textBox3.Text;
+            DB.passwordString = textBox4.Text;
+            DB.tableString = textBox5.Text;
+            DB.buildConnectionString();
+            DB.openConnection();
+            string SQLcommand = "select * from " + DB.tableString + ";";
+            MySqlCommand command = new MySqlCommand(SQLcommand, DB.GetSqlConnection());
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+            DB.closeConnection();
+        }
+
+        private void clearAll() 
+        {
+            table = new DataTable();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.EndEdit();
+            DataTable changedRows = table.GetChanges();
+            if (changedRows !=null)
+            {
+                MySqlCommandBuilder builder = new MySqlCommandBuilder(adapter);
+                adapter.Update(changedRows);
+                table.AcceptChanges();
+                MessageBox.Show("Changes saved");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DB.openConnection();
+            String findString = textBox6.Text;
+            MySqlCommand command = new MySqlCommand();
+            String sqlCommand = "SELECT * FROM " + DB.databaseString + "." + DB.tableString + "WHERE ";
+            String sqlParam;
+            DataGridViewColumn col = dataGridView1.Columns[0];
+            sqlParam = "@" + col.Name;
+            sqlCommand += col.Name + "=" + sqlParam;
+            if(col.ValueType==typeof(int))
+            {
+                command.Parameters.Add(sqlParam, MySqlDbType.Int32).Value = Convert.ToInt32(findString);
+            }
+            else 
+            {
+                command.Parameters.Add(sqlParam, MySqlDbType.VarChar).Value = findString;
+            }
+            command.CommandText = sqlCommand;
+            command.Connection = DB.GetSqlConnection();
+            adapter.SelectCommand = command;
+            clearAll();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+            DB.closeConnection();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resizeContent(object sender, EventArgs e)
+        {
+           // dataGridView1.Width = Width - (dataGridView1.Location.X * 2) - (dataGridView1.Margin.All * 2) - 10; 
+        }
+
+        private void drawButton_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            DB.openConnection();
+            String axisXstring = textboxAX.Text;
+            String axisYstring = textboxAY.Text;
+            DB.tableString = textBox5.Text;
+            table.Clear();
+            MySqlCommand command = new MySqlCommand();
+            string sqlCommand = "Select " + axisYstring + "from" + DB.tableString + ";";
+            command.CommandText = sqlCommand;
+            command.Connection = DB.GetSqlConnection();
+            adapter.SelectCommand = command;
+            DB.closeConnection();
+
+            Graph.Series.Clear();//от сюда гавной воняет
+            Graph.Series.Add("Graph");
+            Graph.Series["Graph"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                string xValue = (table.Rows[i][axisXstring].ToString());
+                double yValue = Double.Parse(table.Rows[i][axisYstring].ToString());
+                Graph.Series["Graph"].Points.AddXY(xValue, yValue);
+            }
+        }
+
+
+    }
+}
